67f919f2e574e34056abf493ff15111e4fce149b
Madhukunchalaa <madhu@smartsolutionsdigi.com>
2025-04-28 10:33:46 +0530

    chnages

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'https://teams-sczd.onrender.com/',
+    origin: 'https://teams-sczd.onrender.com',
     methods: ['GET', 'POST'],
   },
 });
